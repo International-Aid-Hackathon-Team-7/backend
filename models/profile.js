8c9eecc29c5b1a8c02dc3fccb73538e150f306d8
Jan Horak <janny_b_goode@hotmail.com>
2022-05-04 15:25:03 -0400

    update profile model

--- models/profile.js
+++ models/profile.js
@@ -6,17 +6,14 @@ const profileSchema = new Schema(
     name: { type: String, required: true },
     avatar: { type: String },
     email: { type: String, required: true, lowercase: true, unique: true },
-    posts: { type: Schema.Types.ObjectId, ref: "Post" },
-    categories: { type: Schema.Types.ObjectId, ref: "Category" },
-    favorited_posts: { type: Schema.Types.ObjectId, ref: "Post" },
-    location: String,
-    registered_events: { type: Schema.Types.ObjectId, ref: "Post" }
+    posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+    categories: [{ type: Schema.Types.ObjectId, ref: "Category" }],
+    favorited_posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
   },
   {
     timestamps: true,
   }
 );
-
 const Profile = mongoose.model("Profile", profileSchema);
 
 export { Profile };
